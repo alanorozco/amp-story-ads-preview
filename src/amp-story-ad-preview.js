64c8293b70b715140e6ee124805eb82f21fd2f78
Shruthi Santhanam <shruthisanth@google.com>
2019-06-10 15:52:01 -0700

    created iframes structure for preview

--- src/amp-story-ad-preview.js
+++ src/amp-story-ad-preview.js
@@ -13,155 +13,66 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import './amp-story-ad-preview.css';
 import {getNamespace} from '../lib/namespace';
 import {html, render} from 'lit-html';
 import {purifyHtml} from '../lib/runtime-deps/purify-html';
 
 const {n} = getNamespace('amp-story-ad-preview');
 
-const textNodesToStr = nodes =>
-  Array.from(nodes).map(node =>
-    node.nodeType == Node.TEXT_NODE ? node.textContent : node
-  );
-
-function Wrap({childNodes}) {
+function Wrap({iframe}) {
   return html`
-    <!DOCTYPE html>
-    <html amp lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <script async src="https://cdn.ampproject.org/v0.js"></script>
-        <script
-          async
-          custom-element="amp-story"
-          src="https://cdn.ampproject.org/v0/amp-story-1.0.js"
-        ></script>
-        <title>My Story</title>
-        <meta
-          name="viewport"
-          content="width=device-width,minimum-scale=1,initial-scale=1"
-        />
-        <link rel="canonical" href="helloworld.html" />
-        <style amp-boilerplate>
-          body {
-            -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-            -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-            -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-            animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-          }
-          @-webkit-keyframes -amp-start {
-            from {
-              visibility: hidden;
-            }
-            to {
-              visibility: visible;
-            }
-          }
-          @-moz-keyframes -amp-start {
-            from {
-              visibility: hidden;
-            }
-            to {
-              visibility: visible;
-            }
-          }
-          @-ms-keyframes -amp-start {
-            from {
-              visibility: hidden;
-            }
-            to {
-              visibility: visible;
-            }
-          }
-          @-o-keyframes -amp-start {
-            from {
-              visibility: hidden;
-            }
-            to {
-              visibility: visible;
-            }
-          }
-          @keyframes -amp-start {
-            from {
-              visibility: hidden;
-            }
-            to {
-              visibility: visible;
-            }
-          }
-        </style>
-        <noscript
-          ><style amp-boilerplate>
-            body {
-              -webkit-animation: none;
-              -moz-animation: none;
-              -ms-animation: none;
-              animation: none;
-            }
-          </style></noscript
-        >
-        <style amp-custom>
-          body {
-            font-family: 'Roboto', sans-serif;
-          }
-          amp-story-page {
-            background: white;
-          }
-          h1 {
-            font-size: 2.875em;
-            font-weight: normal;
-            line-height: 1.174;
-            text-transform: uppercase;
-          }
-        </style>
-      </head>
-
-      <body>
-        <amp-story standalone>
-          <amp-story-page id="cover">
-            <amp-story-grid-layer template="vertical">
-              <h1>Hello World</h1>
-              <p>This is the cover page of this story.</p>
-            </amp-story-grid-layer>
-          </amp-story-page>
-        </amp-story>
-      </body>
-    </html>
+    <div class="${n('wrap')}">
+      ${iframe}
+    </div>
   `;
-}
-
-function createIframe() {
-  const iframe = this.doc.createElement('iframe');
-  iframe.setAttribute('frameBorder', '0');
-  iframe.setAttribute('id', 'previewIframe');
-  iframe.setAttribute('title', 'AMP Playground Output');
-  iframe.setAttribute('allowpaymentrequest', '');
-  iframe.setAttribute(
-    'sandbox',
-    'allow-scripts allow-forms allow-same-origin allow-popups ' +
-      'allow-popups-to-escape-sandbox allow-presentation allow-top-navigation'
-  );
-  iframe.setAttribute('allowfullscreen', true);
-  iframe.classList.add('elevation-4dp');
-  return iframe;
 }
 
 export default class AmpStoryAdPreview {
   constructor(win, element) {
     this.win = win;
     this.shadow_ = element.attachShadow({mode: 'open'});
+    this.iframe = this.createIframe();
+    const iframe = this.iframe;
+    render(Wrap({iframe}), this.shadow_);
+    this.innerDoc = this.initialize();
+  }
+
+  createIframe() {
+    const iframe = this.win.document.createElement('iframe');
+    iframe.setAttribute('frameBorder', '0');
+    iframe.setAttribute('id', 'previewIframe');
+    iframe.setAttribute('title', 'Preview Output');
+    iframe.setAttribute('allowpaymentrequest', '');
+    iframe.setAttribute(
+      'sandbox',
+      'allow-scripts allow-forms allow-same-origin allow-popups ' +
+        'allow-popups-to-escape-sandbox allow-presentation allow-top-navigation'
+    );
+    iframe.setAttribute('allowfullscreen', true);
+    iframe.classList.add('elevation-4dp');
+    return iframe;
+  }
+
+  initialize() {
+    const head =
+      "<head> <meta charset='utf-8'> <script async src='https://cdn.ampproject.org/v0.js'></script> <script async custom-element='amp-story' src='https://cdn.ampproject.org/v0/amp-story-1.0.js'></script> <title>My Story</title> <meta name='viewport' content='width=device-width,minimum-scale=1,initial-scale=1'> <link rel='canonical' href='helloworld.html'></noscript> <style amp-custom> body { font-family: 'Roboto', sans-serif; } amp-story-page { background: white; } h1 { font-size: 2.875em; font-weight: normal; line-height: 1.174; text-transform: uppercase; } </style> </head>";
+    const defaultBody =
+      "<amp-story standalone> <amp-story-page id='ad'> </amp-story-page> </amp-story>";
+    let doc = this.iframe.contentDocument;
+    doc.head.innerHTML = head;
+    doc.body.innerHTML = defaultBody;
+
+    const innerIframe = this.win.document.createElement('iframe');
+    doc.getElementById('ad').append(innerIframe);
+    return innerIframe.contentDocument;
   }
 
   update(dirty) {
     const body = purifyHtml(dirty, this.win.document);
 
-    // `lit-html` seems to bork when trying to render `TextNodes` as first-level
-    // elements of a `NodeList` part. This maps them to strings as a workaround.
-    // Non-text `Node`s are left as-is.
-    const childNodes = textNodesToStr(body.childNodes);
-
-    const iframe = this.createIframe();
-
-    render(Wrap({childNodes}), this.shadow_);
+    this.innerDoc.open();
+    this.innerDoc.append(body);
+    this.innerDoc.close();
   }
 }
--- src/editor.js
+++ src/editor.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import './amp-story-ad-preview.css';
 import './editor.css';
 import './monokai.css';
 import {getNamespace} from '../lib/namespace';
