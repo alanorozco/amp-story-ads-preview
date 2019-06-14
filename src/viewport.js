dbc5e94ade81f0c408bb18644d70b26eaa767ef2
Shruthi Santhanam <shruthisanth@google.com>
2019-06-14 13:49:23 -0700

    refactored valid viewport check

--- src/viewport.js
+++ src/viewport.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 import './viewport.css';
+import {assertIncludes} from '../lib/assert';
 import {getNamespace} from '../lib/namespace';
 import {html} from 'lit-html';
 import {repeat} from 'lit-html/directives/repeat';
@@ -68,10 +69,7 @@ export const viewportIdFull = 'full';
 export const viewportIdDefault = 'iphone-x';
 
 export function validViewportId(viewportId) {
-  if (!viewportIds.includes(viewportId)) {
-    throw new Error(`Invalid viewport "${viewportId}"`);
-  }
-  return viewportId;
+  return assertIncludes(viewportIds, viewportId, 'Invalid viewport');
 }
 
 export const ViewportSelector = ({selectViewport, viewportId}) => html`
