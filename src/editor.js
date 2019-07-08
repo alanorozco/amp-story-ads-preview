cceb74b4064ffadaa8a3e08f6e015059be18bb6d
bhassan6621 <bibihassan2000@gmail.com>
2019-07-08 10:00:31 -0700

    nit changes

--- src/editor.js
+++ src/editor.js
@@ -383,15 +383,25 @@ const PreviewPanel = ({
   </div>
 `;
 
-const dispatchRefreshIcon = redispatchAs(g('refresh-icon'));
+const dispatchUpdatePreview = redispatchAs(g('update-preview'));
 
-const RefreshIcon = () => svg`
-  <svg @click=${dispatchRefreshIcon} xmlns="http://www.w3.org/2000/svg" cursor="pointer" width="24px" height="24px" viewBox="0 0 24 24" fill="#000000">
-    <g>
-      <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/>
-      <path d="M0 0h24v24H0z" fill="none"/>
-    </g>
-  </svg>
+const RefreshIcon = () => html`
+  <div @click=${dispatchUpdatePreview}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      fill="#000000"
+    >
+      <g>
+        <path
+          d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+        />
+        <path d="M0 0h24v24H0z" fill="none" />
+      </g>
+    </svg>
+  </div>
 `;
 
 /**
@@ -511,7 +521,7 @@ class Editor {
       dragover: this.dragover_,
       drop: this.drop_,
       [g('delete-file')]: this.deleteFile_,
-      [g('refresh-icon')]: this.updatePreview_,
+      [g('update-preview')]: this.updatePreview_,
       [g('insert-file-ref')]: this.insertFileRef_,
       [g('upload-files')]: this.uploadFiles_,
       [g('select-viewport')]: this.selectViewport_,
@@ -657,7 +667,6 @@ class Editor {
   }
 
   updatePreview_() {
-    // debugger;
     const doc = this.codeMirror_.getValue();
     const docWithFileRefs = this.replaceFileRefs_(doc);
     this.preview_.update(docWithFileRefs);
