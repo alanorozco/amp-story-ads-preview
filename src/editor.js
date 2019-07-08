9f810b528a8003a882ab5e66806d38d502c69e19
bhassan6621 <31637947+bhassan6621@users.noreply.github.com>
2019-07-08 19:26:11 -0400

    Refresh button in the preview toolbar (#46)
    placed a svg refresh icon in the preview toolbar that calls updatePreview_ when clicked

--- src/editor.js
+++ src/editor.js
@@ -22,7 +22,7 @@ import {CTA_TYPES} from './cta-types';
 import {Deferred} from '../vendor/ampproject/amphtml/src/utils/promise';
 import {getNamespace} from '../lib/namespace';
 import {hintIgnoreEnds, hintsUrl, setAttrFileHints} from './hints';
-import {html, render} from 'lit-html';
+import {html, render, svg} from 'lit-html';
 import {htmlMinifyConfig} from '../lib/html-minify-config';
 import {identity} from './utils/function';
 import {redispatchAs} from './utils/events';
@@ -384,6 +384,29 @@ const PreviewPanel = ({
   </div>
 `;
 
+const dispatchUpdatePreview = redispatchAs(g('update-preview'));
+
+const RefreshIcon = () => html`
+  <div class="${n('update-preview-button')}" @click=${dispatchUpdatePreview}>
+    ${svg`
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24px"
+        height="24px"
+        viewBox="0 0 24 24"
+        fill="#000000"
+      >
+      <g>
+        <path
+          d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
+        />
+        <path d="M0 0h24v24H0z" fill="none" />
+      </g>
+    </svg>
+    `}
+  </div>
+`;
+
 /**
  * Renders preview panel toolbar.
  * @param {Object} data
@@ -401,6 +424,7 @@ const PreviewToolbar = ({isFullPreview, viewportId}) => html`
       className: [g('flex-center')],
       viewportId,
     })}
+    ${RefreshIcon()}
   </div>
 `;
 
@@ -500,6 +524,7 @@ class Editor {
       dragover: this.dragover_,
       drop: this.drop_,
       [g('delete-file')]: this.deleteFile_,
+      [g('update-preview')]: this.updatePreview_,
       [g('insert-file-ref')]: this.insertFileRef_,
       [g('upload-files')]: this.uploadFiles_,
       [g('select-viewport')]: this.selectViewport_,
