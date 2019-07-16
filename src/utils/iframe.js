d3d00f1a7c147cabc31c8264a65707857b80af5b
bhassan6621 <31637947+bhassan6621@users.noreply.github.com>
2019-07-16 11:39:36 -0700

    cta bug solution (#56)
    There are 3 pages to the story, a "cover" page, "page-1" which contains the story ad, and an additonal story page on "page-2"
    when the page is loaded the preview displays "page-1" which is the ad page, which allows the cta button to go open the link in one click.

--- src/utils/iframe.js
+++ src/utils/iframe.js
@@ -21,7 +21,7 @@ import {Deferred} from '../../vendor/ampproject/amphtml/src/utils/promise';
  * @param {string} srcdoc
  * @return {string} (for compatibility with srcdoc writer, not important atm.)
  */
-function writeToIframe(iframe, srcdoc) {
+export function writeToIframe(iframe, srcdoc) {
   iframe.src = 'about:blank';
   const childDoc = iframe.contentWindow.document;
   childDoc.open();
@@ -47,46 +47,3 @@ export function whenIframeLoaded(iframe) {
   );
   return promise;
 }
-
-/**
- * Writes to an async iframe when dynamic `srcdoc` is unsupported, and
- * establishes dynamic iframe writing support regardless.
- *
- * Result properties are for the callee to handle the iframe based on `srcdoc`
- * support.
- *
- * @param {Window} win
- * @param {Promise<HTMLIframeElement>} iframeReady
- *   Should resolve when the iframe is attached and loaded.
- *   When `document.write()`ing, the iframe will load once more and
- *   chain-resolve into the homonymous property. Otherwise passed through.
- * @param {string} srcdoc
- *   Set on <iframe> once resolved if platform lacks `srcdoc` support.
- *   Otherwise passed through as a result property in order to set as an
- *   attribute on the rendered <iframe>.
- * @return {{
- *   iframeReady: Promise<HTMLIframeElement>,
- *   srcdoc: (string|undefined),
- *   writer: function(HTMLIframeElement, string):string
- * }}
- *  - `iframeReady` resolves when ready for further updates.
- *  - `srcdoc` to set as attribute on `<iframe>` (undefined if unnecessary.)
- *  - `writer` to be used for further updates.
- *    Takes `(iframe, srcdoc)` and returns `srcdoc` for chaining.
- */
-export const setSrcdocAsyncMultiStrategy = (win, iframeReady, srcdoc) =>
-  // https://caniuse.com/#search=srcdoc
-  'srcdoc' in win.HTMLIFrameElement.prototype
-    ? {
-        srcdoc,
-        iframeReady,
-        writer: (iframe, srcdoc) => (iframe.srcdoc = srcdoc),
-      }
-    : {
-        // Writing after attachment, shouldn't set actual srcdoc attribute.
-        iframeReady: iframeReady.then(iframe => {
-          writeToIframe(iframe, srcdoc);
-          return whenIframeLoaded(iframe);
-        }),
-        writer: writeToIframe,
-      };
